2d769ef691823d1e16e4b0b5652a0607a8fe210f
shakshamkaushik <shakshamkaushik1@gmail.com>
2024-05-15 14:20:20 +0530

    Fix Message Status Tick

--- app/src/main/java/com/example/whatsappclone/activities/SingleChatActivity.kt
+++ app/src/main/java/com/example/whatsappclone/activities/SingleChatActivity.kt
@@ -224,7 +224,7 @@ class SingleChatActivity : AppCompatActivity(), TextWatcher {
 
         if (currentUSerId != null && isReadReceiptEnabled!!) {
             chats.forEachIndexed { index, chatMessage ->
-                if (chatMessage.getReceiverUid() == currentUSerId && !chatMessage.getIsMessageSeen()) {
+                if (chatMessage.getReceiverUid() == currentUSerId && chatMessage.getIsMessageSeen()) {
                     val messageId = chatIds[index]
                     messageCollectionRef.document(messageId).update("isMessageSeen", true)
                 }
